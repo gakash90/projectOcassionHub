ec51f78bbdb5c6c750008130a762e77b66253a84
gakash90 <gakashw@gmail.com>
2025-01-16 17:51:47 +0530

    env file changes

---
+++ src/services/userServices.js
@@ -0,0 +1,32 @@
+
+import User from "../models/user.model.js";
+import logger from "../utils/logger.js";
+
+export const userService = async(userData)=>{
+    try {
+        const {username, email, password} = userData
+        logger.info(`Checking if user exists: username=${username}, email=${email}`);    
+        const isExisting = await User.findOne({email})
+        if(isExisting){
+            logger.error(`user already registered ${email}`)
+            throw new Error("User already registered")
+        }
+        
+        const newUser = User({
+            username,
+            email,
+            password
+        })
+       
+        await newUser.save()
+
+        logger.info(`User registered Successfully ${email}`)
+
+        return newUser
+    } 
+        catch (error) {
+
+            logger.error(`Error in the registering user ${error.message}`, error)
+            throw new Error(`Error in Regsitering the User ${error.message}`)
+    }
+}
